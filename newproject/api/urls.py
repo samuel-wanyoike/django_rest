f41b7a0ff738cd8a55212e1fb37f987521df6e4e
samuel wanyoike <wanyoikesamuel42@gmail.com>
2024-09-29 00:47:00 +0300

    create crud endpoints

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_user, get_user
+from .views import create_user, get_users, user_detail
 
 urlpatterns = [
-    path('users/', get_user, name='get_user' ),
-    path('users/create/', create_user, name='create_user')
+    path('users/', get_users, name='get_users' ),
+    path('users/create/', create_user, name='create_user'),
+    path('users/<int:pk>', user_detail, name='create_user')
 ]
--- newproject/api/views.py
+++ newproject/api/views.py
@@ -7,8 +7,10 @@ from .serializer import UserSerializer
 # Create your views here.
 
 @api_view(['GET'])
-def get_user(request):
-    return Response(UserSerializer({'name': 'wanyoike', 'age': 24}).data)
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def create_user(request):
@@ -17,3 +19,25 @@ def create_user(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
